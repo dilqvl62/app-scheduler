aefd146f80aea2934e0017fa3151b5114299b242
sabar mouri <sab38m@hotmail.com>
2022-06-15 13:51:57 -0500

    starting working on courseAction

--- client/src/components/course/CourseAction.js
+++ client/src/components/course/CourseAction.js
@@ -1,4 +1,19 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
 import TabActionForm from '../TabActionForms'; 
+
+
+const CourseAction = (props) => {
+    const { addCourse, deleteCourse, selected,values, setters} = props;
+    const { setCourseTitle, setCourseCode, setCourseLength } = setters;
+    const {courseTitle, courseCode, courseLength } = values;
+    const isAddButtonDisabled = !(courseTitle && courseCode && courseLength)
+
+    const textFields = ([]
+
+    )
+
+
+}
