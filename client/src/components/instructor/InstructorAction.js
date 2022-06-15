43b6c1ebfb827edf568bf9894d2bf683aa486acc
Sabar <smtcg@umsystem.edu>
2022-06-14 23:41:21 -0500

    updated InstructorAction

--- client/src/components/instructor/InstructorAction.js
+++ client/src/components/instructor/InstructorAction.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import TabActionForm from '../TabActionForms';
+import TabActionForm from '../TabActionForms';                                                                                                                                                                                                                                                                                                                                                                                                                                    
 
 const InstructorAction = (props) => {
     const { addInstructor, deleteInstructor, selected, values, setters } = props;
